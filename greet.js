a123dd01b7abdc2c87f9619192e0b8ccc0e133b3
Craig Dunlop <craigdun9393@gmail.com>
2018-10-25 12:42:08 +0100

    added bunch of functions

--- greet.js
+++ greet.js
@@ -1,2 +1,11 @@
 const greet = (name = "World") => `Hello ${name}!`;
 return "does this work"
+
+const HelloWorld = (name = "World") => `Hello ${name}!`;
+return "does this work"
+
+const greet = (name = "World") => `Hello ${name}!`;
+return "does this work"
+
+const greet = (name = "World") => `Hello ${name}!`;
+return "does this work"
